850574d4293e46cc3ff63bb9febd96a829d45256
CARLO BRUNO B. BARRAGAN FIGUEROA <Carlo.Barragan@ibm.com>
2018-04-19 12:29:58 -0500

    standardized code to have semicolons

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const path_template = require('./templates/path').template;
 const createPath = require('./auxFunctions/pathManipulation').createPath;
 
 
-const filename = process.argv[3] || 'RAPI_Docs.yml'
+const filename = process.argv[3] || 'RAPI_Docs.yml';
 const input = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 
 let swagger = {
@@ -21,45 +21,45 @@ let swagger = {
   'schemes': [],
   'paths': {},
   'definitions': {}
-}
+};
 
 const buildFormData = formdata => {
-  let params = []
+  let params = [];
   formdata.forEach(form => {
     let param = JSON.parse(params_template);
     param.in = 'formData';
     param.name = form.key;
     param.required = (!form.disabled) ? true : false;
     params.push(param);
-  })
+  });
   return params;
-}
+};
 
 const buildRaw = (rawData, name) => {
-  let params = []
+  let params = [];
   let param = JSON.parse(body_template);
   rawData = JSON.parse(rawData);
-  param.in = 'body'
-  param.name = 'body'
+  param.in = 'body';
+  param.name = 'body';
   param.required = (!rawData.__required || rawData.__required === true) ? true : false;
-  delete rawData.__required
-  param.schema.$ref = param.schema.$ref.concat(name)
-  createDefinition(swagger.definitions, name, rawData)
+  delete rawData.__required;
+  param.schema.$ref = param.schema.$ref.concat(name);
+  createDefinition(swagger.definitions, name, rawData);
   params.push(param);
   return params;
-}
+};
 
 const generateParams = (body, name) => {
   switch (body.mode) {
     case 'formdata':
-      return buildFormData(body.formdata)
+      return buildFormData(body.formdata);
     case 'raw':
       if (body.raw === "")
-        return undefined
-      return buildRaw(body.raw, name)
+        return undefined;
+      return buildRaw(body.raw, name);
     default:
-      return undefined
-  }
+      return undefined;
+  };
 };
 
 
@@ -67,18 +67,17 @@ const createResponses = (method, responses) => {
   if (responses.length === 0)
     method[200] = {
       'description': 'ok',
-    }
+    };
   else
     responses.forEach(response => {
       let body;
       try {
-        body = JSON.parse(response.body)
+        body = JSON.parse(response.body);
       } catch (err) {
         method[response.code] = {
           'description': response.body
-        }
+        };
         return;
-
       }
       if (Array.isArray(body)) {
         method[response.code] = {
@@ -89,35 +88,35 @@ const createResponses = (method, responses) => {
               '$ref': `#/definitions/${response.name}_resp`
             }
           }
-        }
-        createDefinition(swagger.definitions, `${response.name}_resp`, body[0])
+        };
+        createDefinition(swagger.definitions, `${response.name}_resp`, body[0]);
       } else {
         method[response.code] = {
           'description': response.status,
           'schema': {
             '$ref': `#/definitions/${response.name}_resp`
           }
-        }
-        createDefinition(swagger.definitions, `${response.name}_resp`, body)
+        };
+        createDefinition(swagger.definitions, `${response.name}_resp`, body);
       }
-    })
-}
+    });
+};
 
 
 const createDefinition = (path, name, value) => {
   const type = (Array.isArray(value)) ? 'array' : typeof value;
   switch (type) {
     case 'boolean':
-      path.type = 'boolean'
-      path.example = value
+      path.type = 'boolean';
+      path.example = value;
       break;
     case 'number':
-      path.type = 'number'
-      path.example = value
+      path.type = 'number';
+      path.example = value;
       break;
     case 'string':
-      path.type = 'string'
-      path.example = value
+      path.type = 'string';
+      path.example = value;
       break;
     case 'array':
       path[name] = {};
@@ -143,7 +142,7 @@ const createDefinition = (path, name, value) => {
         path[name] = newPath;
       break;
     case 'undefined':
-      path.type = 'string'
+      path.type = 'string';
       break;
     default:
       console.log('Missing Case for type: ', type);
@@ -162,8 +161,8 @@ exports.main = (() => {
       let subAux = req.request.url.path || [' '];
       let newPath = createPath(subAux);
       if (!swagger.paths[newPath])
-        swagger.paths[newPath] = {}
-      let method = JSON.parse(path_template)
+        swagger.paths[newPath] = {};
+      let method = JSON.parse(path_template);
 
       method.tags = [path.name];
       method.summary = req.request.description;
@@ -171,23 +170,23 @@ exports.main = (() => {
       if (req.request.header.length != 0)
         method.consumes = [req.request.header[0].value];
       if (swagger.schemes.indexOf(req.request.url.protocol) === -1 && req.request.url.protocol != undefined)
-        swagger.schemes.push(req.request.url.protocol)
-      let name = `${subAux.join('_')}_${req.request.method}`
+        swagger.schemes.push(req.request.url.protocol);
+      let name = `${subAux.join('_')}_${req.request.method}`;
       if (req.request.body.mode === 'formdata')
-        method.consumes.push('multipart/form-data')
-      method.parameters = generateParams(req.request.body, name)
-      createResponses(method.responses, req.response)
+        method.consumes.push('multipart/form-data');
+      method.parameters = generateParams(req.request.body, name);
+      createResponses(method.responses, req.response);
       if (method.parameters === undefined)
-        delete method.parameters
+        delete method.parameters;
       if (method.consumes.length === 0)
-        delete method.consumes
+        delete method.consumes;
       if (method.summary === undefined)
-        delete method.summary
+        delete method.summary;
       if (method.produces.length === 0)
-        method.produces.push('application/json')
+        method.produces.push('application/json');
       if (Object.keys(method.responses).length === 0)
         console.error('No Responses Found');
-      swagger.paths[newPath][req.request.method.toLowerCase()] = method
+      swagger.paths[newPath][req.request.method.toLowerCase()] = method;
 
 
     })
